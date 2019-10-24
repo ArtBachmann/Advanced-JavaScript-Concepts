192b0a8f8b8915de0e6d0738220971bc750d8a3b
Art Bachmann <bachmann.art@gmail.com>
2019-10-24 17:18:43 +0300

    started repeating important concepts

---
+++ Section1/introduction.md
@@ -0,0 +1,4 @@
+2008 v8 was released. What is inside?
+
+Brendan Eich SpiderMonkey.Noe in Firefox.
+Javascript.
---
+++ Section13/array.js
@@ -0,0 +1,6 @@
+const strings = ['1', '2', '3', '4']
+// 4*4 = 16 bytes of memory
+
+console.log(strings[2])
+
+// push, pop, 
